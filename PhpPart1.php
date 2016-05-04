a9de01fd491e666a90bf447df5f3a1371b9be817
Abigail Barrios <Abigail Barrios>
2016-05-03 20:44:21 -0400

    first commit

---
+++ PhpPart1.php
@@ -0,0 +1,56 @@
+
+<?php
+require("dbinfo.php");
+
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
+
+
+$connection=mysql_connect ('localhost', $username, $password);
+if (!$connection) {
+  die('Not connected : ' . mysql_error());
+}
+
+
+$db_selected = mysql_select_db($database, $connection);
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mysql_error());
+}
+
+
+$query = "SELECT * FROM markers WHERE 1";
+$result = mysql_query($query);
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
+}
+
+header("Content-type: text/xml");
+
+
+echo '<markers>';
+
+
+while ($row = @mysql_fetch_assoc($result)){
+
+  echo '<marker ';
+  echo 'name="' . parseToXML($row['name']) . '" ';
+  echo 'address="' . parseToXML($row['address']) . '" ';
+  echo 'lat="' . $row['lat'] . '" ';
+  echo 'lng="' . $row['lng'] . '" ';
+  echo 'type="' . $row['type'] . '" ';
+  echo '/>';
+}
+
+// End XML file
+echo '</markers>';
+
+?>
+</body>
+</html>
